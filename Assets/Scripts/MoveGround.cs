e323ac500c477a8dfdbcd5d9695f9fb9b74adf36
Arquery <sgu_sigu@msn.com>
2020-01-21 14:38:31 -0700

    Creating level 1
    Were added the player sprite in level 1 and your idle animation, also was created scripts for platforms movement.

---
+++ Assets/Scripts/MoveGround.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveGround : MonoBehaviour
+{
+    [Range(-30.0f, 0.0f)]
+    public float moveSpeed =-5f;
+    public Grid tilemap;
+   
+    private float finalSpeed;
+    private bool move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = false;
+    }
+
+    // Update is called once per frame
+   void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            move = true;
+        }
+
+        if (move == true)
+        {
+            ParallaxMoveGround();
+        }
+    }
+
+    private void ParallaxMoveGround()
+    {
+        finalSpeed = moveSpeed + finalSpeed;
+        tilemap.transform.position = new Vector3(finalSpeed, tilemap.transform.position.y);
+    }
+}
